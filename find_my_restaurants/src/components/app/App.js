82bc33f9f97a6eccb51a146ae02edc2ab88a47e3
Rohit Anand <rohitanand3456@gmail.com>
2020-08-14 21:44:51 +0530

    added basic app and index comnponent and styling

---
+++ find_my_restaurants/src/components/app/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Classes from './App.module.scss';
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <header>
+                
+            </header>
+
+            <main>
+            </main>    
+            
+            <footer>
+
+
+            </footer>
+        </React.Fragment>
+    );
+}
